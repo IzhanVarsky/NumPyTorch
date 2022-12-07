cbb7a77743b8069ba968e001a75c71e1bae01169
IzhanVarsky <48652648+IzhanVarsky@users.noreply.github.com>
2022-12-07 23:48:15 +0300

    reduction policy for losses added

--- nn/loss.py
+++ nn/loss.py
@@ -4,15 +4,23 @@ from .module import Module
 
 
 class Loss(Module, ABC):
+    def __init__(self, reduction: str = 'mean'):
+        super(Loss, self).__init__()
+        possible_reductions = ['none', 'mean', 'sum']
+        if reduction not in possible_reductions:
+            print(f"Warning: `{reduction}` reduction is unknown. "
+                  f"`mean` reduction will be used instead.")
+            reduction = 'mean'
+        self.reduction = reduction
+
     @abstractmethod
     def backward(self):
         raise NotImplementedError()
 
 
 class CrossEntropyLoss(Loss):
-    def __init__(self, label_smoothing=0.0):
-        super(CrossEntropyLoss, self).__init__()
-        # TODO: add reduction policy
+    def __init__(self, reduction: str = 'mean', label_smoothing=0.0):
+        super(CrossEntropyLoss, self).__init__(reduction)
         self.label_smoothing = label_smoothing
         self.cached_input = None
         self.cached_target = None
@@ -22,22 +30,52 @@ class CrossEntropyLoss(Loss):
         bz, k = x.shape
         self.cached_input = x.clip(min=1e-8, max=None)  # to avoid division by zero
         self.cached_target = target * (1 - self.label_smoothing) + self.label_smoothing / k
-        return -(self.cached_target * np.log(self.cached_input)).sum(axis=1)
+        out = -(self.cached_target * np.log(self.cached_input)).sum(axis=1)
+        if self.reduction == 'mean':
+            return out.mean()
+        if self.reduction == 'sum':
+            return out.sum()
+        return out
 
     def backward(self):
-        return -self.cached_target / self.cached_input / self.cached_input.shape[0]
+        reduction = self.reduction
+        if self.reduction == 'none':
+            print(f"Warning: backward for `none` reduction is not defined. "
+                  f"`mean` reduction is used instead.")
+            reduction = 'mean'
+        out = -self.cached_target / self.cached_input
+        if reduction == 'mean':
+            return out / self.cached_input.shape[0]
+        if reduction == 'sum':
+            return out
+        return out
 
 
 class MSELoss(Loss):
-    def __init__(self):
-        super(MSELoss, self).__init__()
+    def __init__(self, reduction: str = 'mean'):
+        super(MSELoss, self).__init__(reduction)
         self.cached_input = None
         self.cached_target = None
 
     def forward(self, x, target):
         self.cached_input = x
         self.cached_target = target
-        return ((target - x) ** 2).mean(axis=1)
+        out = ((target - x) ** 2)
+        if self.reduction == 'mean':
+            return out.mean()
+        if self.reduction == 'sum':
+            return out.sum()
+        return out
 
     def backward(self):
-        return 2 * (self.cached_input - self.cached_target) / np.prod(self.cached_input.shape)
+        reduction = self.reduction
+        if self.reduction == 'none':
+            print(f"Warning: backward for `none` reduction is not defined. "
+                  f"`mean` reduction is used instead.")
+            reduction = 'mean'
+        out = 2 * (self.cached_input - self.cached_target)
+        if reduction == 'mean':
+            return out / np.prod(self.cached_input.shape)
+        if reduction == 'sum':
+            return out
+        return out
--- tests/nn/loss.py
+++ tests/nn/loss.py
@@ -13,17 +13,20 @@ class TestLosses(unittest.TestCase):
         softmax = nn.Softmax()
         in_shape = (12, 10)
         smoothing = 0.3
+        reduction = 'mean'
+
         input = np.random.random(in_shape)
         target = softmax(np.random.random(in_shape))
 
         torch_dtype = torch.float32
         t_input = torch.tensor(input, dtype=torch_dtype, requires_grad=True)
         t_target = torch.tensor(target, dtype=torch_dtype)
-        torch_layer = torch.nn.CrossEntropyLoss(label_smoothing=smoothing, reduction='none')
+        torch_layer = torch.nn.CrossEntropyLoss(reduction=reduction, label_smoothing=smoothing)
         t_out = torch_layer(t_input, t_target)
-        t_out.mean().backward(retain_graph=True)
+        t_out.backward(retain_graph=True)
 
-        my_layer = nn.Net(nn.Softmax(), nn.CrossEntropyLoss(smoothing))
+        my_layer = nn.Net(nn.Softmax(),
+                          nn.CrossEntropyLoss(reduction=reduction, label_smoothing=smoothing))
         my_out = my_layer(input)
         my_out = my_layer.loss(my_out, target)
         my_grad_out = my_layer.backward()
@@ -37,21 +40,22 @@ class TestLosses(unittest.TestCase):
 
     def test_rand_mse_loss(self):
         in_shape = (3, 7, 6)
+        reduction = 'mean'
         input = np.random.random(in_shape)
         target = np.random.random(in_shape)
 
         torch_dtype = torch.float32
         t_input = torch.tensor(input, dtype=torch_dtype, requires_grad=True)
         t_target = torch.tensor(target, dtype=torch_dtype)
-        torch_layer = torch.nn.MSELoss()
+        torch_layer = torch.nn.MSELoss(reduction=reduction)
         t_out = torch_layer(t_input, t_target)
         t_out.backward(retain_graph=True)
 
-        my_layer = nn.MSELoss()
+        my_layer = nn.MSELoss(reduction=reduction)
         my_out = my_layer(input, target)
         my_grad_out = my_layer.backward()
 
-        assert_allclose(my_out.mean(), t_out.detach().numpy(), atol=0.0001,
+        assert_allclose(my_out, t_out.detach().numpy(), atol=0.0001,
                         err_msg="---- Forward: test failed :(")
         print("++++ Forward: test passed!")
         assert_allclose(my_grad_out, t_input.grad.numpy(),
